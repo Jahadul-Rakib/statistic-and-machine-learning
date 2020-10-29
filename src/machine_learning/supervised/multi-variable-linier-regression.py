161d73ef690bc165d09b097d233b8ebb82b8dbb8
rakib <jhadul.islam@itconquest.com>
2020-10-29 22:10:29 +0600

    add'

--- src/machine_learning/supervised/multi-variable-linier-regression.py
+++ src/machine_learning/supervised/multi-variable-linier-regression.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn import model_selection, linear_model
 
 data = pd.read_csv('C:/Users/itc/PycharmProjects/scientificProject/data/car_prices.csv')
@@ -9,6 +10,14 @@ data.Mileage = data.Mileage.fillna(mileage)
 
 x = data[['Mileage', 'Age']]
 y = data['Sell Price']
+
+'''
+plt.scatter(*data['Mileage', 'Age'], data['Sell Price'])
+plt.xlabel('x axis for speed')
+plt.ylabel('y axis for risk')
+plt.title('Risk of speed')
+plt.show()
+'''
 
 x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.3, random_state=2)
 model = linear_model.LinearRegression()
@@ -21,7 +30,7 @@ def predict_price():
     x = input('please enter Mileage: ')
     y = input('please enter Age: ')
     price = model.predict([[int(x), int(y)]])
-    print("Predicted Price is ", price, " for milage and age----->", x,"   ",y)
+    print("Predicted Price is ", price, " for milage and age----->", x, "   ", y)
     return predict_price()
 
 
