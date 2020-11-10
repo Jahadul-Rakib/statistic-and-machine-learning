31fff2fd55135b97fe251469bc9e2833ee3b1cef
rakib <jhadul.islam@itconquest.com>
2020-11-10 20:32:10 +0600

    add

--- src/machine_learning/supervised/encoding-count-victories.py
+++ src/machine_learning/supervised/encoding-count-victories.py
@@ -1,2 +1,15 @@
 from sklearn.feature_extraction.text import CountVectorizer
 
+data = ['I Love Bangladesh',
+        'My name is Rakib',
+        'I am a Java developer',
+        'My Father Name is Tariqual Islam']
+
+# CountVectorizer will use when my dataset will become a meaningful sentence
+cv = CountVectorizer()
+# transform data to row and column
+x = cv.fit_transform(data)
+# get feature name from text dataset
+feature = cv.get_feature_names()
+
+x_array = x.toarray()
