23c9703284fea09fbff2f3f12017e2f161e62faf
rakib <jhadul.islam@itconquest.com>
2020-11-14 12:13:46 +0600

    add

---
+++ src/machine_learning/supervised/RandomForenst/random_forest_regression.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+
+data = pd.read_csv('C:/Users/itc/PycharmProjects/scientificProject/data/credit_card_taiwan.csv')
+print('total row and column: ', data.shape)
+print('total null value: ', data.isnull().sum())
+
+mean_age = data.AGE.mean()
+data.AGE = data.AGE.fillna(mean_age)
+
+y = data['default.payment.next.month']
+x = data.drop(['default.payment.next.month'], axis=1)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=1)
+
+# n_estimator define how many subset tree will make
+model = RandomForestRegressor(n_estimators=200)
+model.fit(x_train, y_train)
+
+accuracy = model.score(x_test, y_test)
+print('Accuracy: ', accuracy)
